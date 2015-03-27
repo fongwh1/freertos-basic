e3d173bb2c782cab1393ba93ed0fcaecc4898464
Will Fong <fongwh1016@gmail.com>
2015-03-27 22:29:15 +0800

    romfs.h: define 'FILE_NAME_LEN'
        define the maximum of a filename stored in d_name

--- include/romfs.h
+++ include/romfs.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #define DIR_LEN 128
+#define FILE_NAME_LEN 128
 
 void register_romfs(const char * mountpoint, const uint8_t * romfs);
 const uint8_t * romfs_get_file_by_hash(const uint8_t * romfs, uint32_t h, uint32_t * len);
