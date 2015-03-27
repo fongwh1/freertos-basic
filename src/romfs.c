a5dc4c68728f2a9b767710ad8051182c0d6f2c43
Will Fong <fongwh1016@gmail.com>
2015-03-27 22:31:24 +0800

    romfs.c: add romfs_dir_next, romfs_close, romfs_opendir
        the romfs_opendir is the register function for dir, to declare the
    dirnext, dirclose function of romfs to be the romfs_dir_next,
    romfs_dir_close;
        romfs_dir_next is used for readdir(dirent) to return the filename of
    dirent and to go to the next entry;
        romfs_dir_close is for the exit of finished opendir();
    
        romfs_dir_next and romfs_dir_close is the implemetation requirement
    of command 'ls'.

--- src/romfs.c
+++ src/romfs.c
@@ -8,6 +8,7 @@
 #include "osdebug.h"
 #include "hash-djb2.h"
 #include "dir.h"
+#include "string.h"
 
 #define hash_init 5381
 
@@ -19,7 +20,7 @@ struct romfs_fds_t {
 
 struct romfs_dirent_t {
     uint32_t d_ino; /* unused so far*/
-    char d_name[128]; /* dir name, assume the name of a file
+    char d_name[FILE_NAME_LEN]; /* dir name, assume the name of a file
                          would be shorter than 128 charaters*/
 };
 
@@ -27,7 +28,7 @@ struct romfs_dirs_t {
     uint32_t start;
     uint32_t cur_off;
     uint32_t end;
-}
+};
 
 static struct romfs_fds_t romfs_fds[MAX_FDS];
 
@@ -82,6 +83,70 @@ static off_t romfs_seek(void * opaque, off_t offset, int whence) {
     return offset;
 }
 
+int romfs_dir_next(void * opaque, void * buf, size_t bufsize) {
+    struct romfs_dirs_t * dirs = opaque;
+    if ((dirs->cur_off + dirs->start) > dirs->end){
+        return 0;
+    }
+    char * pBuf = buf;
+
+    uint32_t eff_offset = dirs->start + dirs->cur_off;
+    size_t i = bufsize;
+    uint32_t c = 0;
+    while ((*pBuf++ = romfs_dirent[eff_offset].d_name[c++]) && i > 1) i--;
+    *(pBuf) = '\0';
+    dirs->cur_off ++;
+    return (bufsize - i); //or return 0 would be better?
+    
+}
+
+int romfs_dir_close(void * opaque) {
+    uint32_t i, j;
+    struct romfs_dirs_t * dirs = opaque;
+
+    for (i = dirs->start; i <= dirs->end; i++){
+        j = 0;
+        romfs_dirent[i].d_ino = 0;
+        while(j++<FILE_NAME_LEN){
+        romfs_dirent[i].d_name[j] = '\0';
+        }
+    }
+
+    return 0;
+}
+
+int romfs_get_all_filename_by_hash(const uint8_t * romfs, uint32_t h, uint32_t start) {
+    const uint8_t * meta;
+    uint32_t i = start;
+    for (meta = romfs; get_unaligned(meta) && get_unaligned(meta + 4); meta += get_unaligned(meta + 4) + 12) {
+        if (get_unaligned(meta+8) == h){
+            strcpy(romfs_dirent[i].d_name, (const char *)meta+12); //may overflow the d_name
+            romfs_dirent[i].d_ino = (i - start + 1);
+            i++;
+        }
+    }
+    return (i - start);
+}
+
+int romfs_opendir(void * opaque, const char * path){
+    uint32_t hash = hash_djb2((const uint8_t *) path, -1);
+    const uint8_t * romfs = (const uint8_t *)opaque;
+    uint32_t start = 0; // assume only one dir at a time,
+                        // so dirent[] always start at 0;
+    uint32_t matched = romfs_get_all_filename_by_hash(romfs, hash, start);
+    uint32_t end = 0, dird = 0;
+    if (matched > 0){
+        end = start + matched;
+        dird = dir_open(romfs_dir_next, romfs_dir_close, opaque);
+        romfs_dirs[dird].start = start;
+        romfs_dirs[dird].cur_off = 0;
+        romfs_dirs[dird].end = end;
+        dir_set_opaque(dird, romfs_dirs + dird);
+    }
+
+    return dird;
+}
+
 const uint8_t * romfs_get_file_by_hash(const uint8_t * romfs, uint32_t h, uint32_t * len) {
     const uint8_t * meta;
 
@@ -94,10 +159,6 @@ const uint8_t * romfs_get_file_by_hash(const uint8_t * romfs, uint32_t h, uint32
         }
     }
 
-    return NULL;
-}
-
-int romfs_opendir(void * opaque, const char * fname){
     return NULL;
 }
 
